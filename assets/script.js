229457d4a062aa9c3be4c5beee18d2eb385eae68
Sangam Biradar <sangam14@users.noreply.github.com>
2021-05-03 20:19:28 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+(
+  function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) }
+            
+})()
